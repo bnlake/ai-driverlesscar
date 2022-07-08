41f19347b3c1c43c9a968545d5ebbaf06c07b905
Brian Lake <bnlake@gmail.com>
2022-07-07 20:44:13 -0500

    convert to using unit circle physics for direction

--- src/car.ts
+++ src/car.ts
@@ -4,8 +4,10 @@ export default class Car {
 	controls: Controls;
 	speed: number;
 	maxSpeed: number = 3;
+	angle = 0;
 
 	rateOfAcceleration: number;
+	rateOfTurn: number = 0.05;
 	friction: number = 0.05;
 
 	constructor(public x: number, public y: number, public width: number, public height: number) {
@@ -17,20 +19,33 @@ export default class Car {
 	draw(ctx: CanvasRenderingContext2D | null) {
 		if (!ctx) return;
 
+		ctx.save();
+		ctx.translate(this.x, this.y);
+		ctx.rotate(-this.angle);
+
 		ctx.beginPath();
-		ctx.rect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+		ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
 		ctx.fill();
+
+		ctx.restore();
 	}
 
 	update() {
 		if (this.controls.forward && this.speed < this.maxSpeed) this.speed += this.rateOfAcceleration;
 		if (this.controls.reverse && this.speed > -this.maxSpeed) this.speed -= this.rateOfAcceleration;
 
+		if (this.speed !== 0) {
+			const flip = this.speed > 0 ? 1 : -1;
+			if (this.controls.left) this.angle += this.rateOfTurn * flip;
+			if (this.controls.right) this.angle -= this.rateOfTurn * flip;
+		}
+
 		if (this.speed > 0) this.speed -= this.friction;
 		if (this.speed < 0) this.speed += this.friction;
 
 		if (Math.abs(this.speed) <= this.friction) this.speed = 0;
 
-		this.y -= this.speed;
+		this.x -= Math.sin(this.angle) * this.speed;
+		this.y -= Math.cos(this.angle) * this.speed;
 	}
 }
