f030a9afe02ed3c5368c979dc3fb8a7d1cb25faf
Brian Lake <bnlake@gmail.com>
2022-07-14 17:01:52 -0500

    add canvas and size it for network

--- src/main.ts
+++ src/main.ts
@@ -3,33 +3,39 @@ import { ControlType } from './controls';
 import Road from './road';
 import './style.css';
 
-const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+const carCanvas = document.getElementById('carCanvas') as HTMLCanvasElement;
+const networkCanvas = document.getElementById('networkCanvas') as HTMLCanvasElement;
+
 const laneCount = 6;
 const laneWidth = 70;
 const laneMargin = 20;
 const carWidth = laneWidth - laneMargin;
 const carHeight = laneWidth * 1.6;
 
-canvas.width = laneCount * laneWidth + 20;
+carCanvas.width = laneCount * laneWidth + 20;
+carCanvas.width = 300;
 
-const ctx = canvas.getContext('2d');
-const road = new Road(canvas.width / 2, laneCount, laneWidth);
+const carCtx = carCanvas.getContext('2d');
+const networkCtx = networkCanvas.getContext('2d');
+
+const road = new Road(carCanvas.width / 2, laneCount, laneWidth);
 const car = new Car(road.getLaneCenter(3), 100, carWidth, carHeight, ControlType.AI, 6);
 const traffic: Array<Car> = [new Car(road.getLaneCenter(1), -100, carWidth, carHeight, ControlType.None)];
 
 function animate() {
-	canvas.height = window.innerHeight;
+	carCanvas.height = networkCanvas.height = window.innerHeight;
 	for (const car of traffic) car.update(road, []);
 	car.update(road, traffic);
 
-	ctx?.save();
-	ctx?.translate(0, -car.y + canvas.height * 0.7);
+	carCtx?.save();
+	carCtx?.translate(0, -car.y + carCanvas.height * 0.7);
 
-	road.draw(ctx);
+	road.draw(carCtx);
 
-	for (const car of traffic) car.draw(ctx, 'red');
-	car.draw(ctx, 'blue');
-	ctx?.restore();
+	for (const car of traffic) car.draw(carCtx, 'red');
+	car.draw(carCtx, 'blue');
+	carCtx?.restore();
+
 	requestAnimationFrame(animate);
 }
 
